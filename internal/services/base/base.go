f895f3d5263f9cecbbd570c234ef8b38888e18da
danbai <danbai@88.com>
2022-11-02 09:58:16 +0800

    server

--- internal/services/base/base.go
+++ internal/services/base/base.go
@@ -1,37 +1,46 @@
 package base
 
+import (
+	"sync"
+)
+
 type RegServer interface {
 	CheckSever() error
 	OverServer() error
 }
 type ServerManage struct {
-	sMap map[string]RegServer
+	sMap sync.Map
 }
 
 func NewServerManage() *ServerManage {
-	return &ServerManage{sMap: make(map[string]RegServer)}
+	return &ServerManage{sMap: sync.Map{}}
 }
 func (m *ServerManage) Get(name string) interface{} {
-	return m.sMap[name]
+	value, _ := m.sMap.Load(name)
+	return value
 }
 func (m *ServerManage) Set(name string, sever RegServer) {
-	m.sMap[name] = sever
+	m.sMap.Store(name, sever)
 }
 func (m *ServerManage) Check() error {
-	for _, v := range m.sMap {
-		err := v.CheckSever()
+	var err error
+	m.sMap.Range(func(key, value any) bool {
+		err = value.(RegServer).CheckSever()
 		if err != nil {
-			return err
+			return false
 		}
-	}
-	return nil
+		return true
+	})
+	return err
 }
 func (m *ServerManage) Over() error {
-	for _, v := range m.sMap {
-		err := v.OverServer()
+	var err error
+	m.sMap.Range(func(key, value any) bool {
+		err = value.(RegServer).OverServer()
 		if err != nil {
-			return err
+			return false
 		}
-	}
-	return nil
+		return true
+	})
+	return err
 }
